7998411878f86f148233b9f2b926f0f8d44f865f
jchen302023 <jchen30@stuy.edu>
2022-01-06 20:39:13 -0500

    neatneesss :)))

---
+++ Driver.java
@@ -0,0 +1,101 @@
+import java.util.ArrayList;
+
+public class Driver {
+  public static void main(String[] args) {
+    ArrayList increasing = new ArrayList<Integer>();
+    increasing.add(1);
+    increasing.add(2);
+    increasing.add(3);
+    increasing.add(4);
+    increasing.add(5);
+    ArrayList decreasing = new ArrayList<Integer>();
+    decreasing.add(5);
+    decreasing.add(4);
+    decreasing.add(3);
+    decreasing.add(2);
+    decreasing.add(1);
+    ArrayList unordered = new ArrayList<Integer>();
+    unordered.add(7);
+    unordered.add(1);
+    unordered.add(5);
+    unordered.add(12);
+    unordered.add(3);
+
+    // bubble sort ==========================
+    // == Best Case == //
+    // 0 swaps neccessary
+    System.out.println("\n==BubbleSort==");
+
+
+    System.out.println( "ArrayList increasing before sorting:\n" + increasing );
+    Sorts.bubble(increasing);
+    System.out.println( "ArrayList increasing after sorting:\n" + increasing );
+    System.out.println("======================");
+    // == Worst Case Scenario == //
+    // 10 swaps neccessary
+
+    System.out.println( "ArrayList decreasing before sorting:\n" + decreasing );
+    Sorts.bubble(decreasing);
+    System.out.println( "ArrayList decreasing after sorting:\n" + decreasing );
+    System.out.println("======================");
+    // == Unordered Case Scenario == //
+    // 5 swaps
+
+    System.out.println( "ArrayList unordered before sorting:\n" + unordered );
+    Sorts.bubble(unordered);
+    System.out.println( "ArrayList unordered after sorting:\n" + unordered );
+    System.out.println("======================");
+
+
+    // insertion sort =========================
+    // == Best Case == //
+    // 0 swaps neccessary
+    System.out.println("\n==InsertionSort==");
+
+    System.out.println( "ArrayList increasing before sorting:\n" + increasing );
+    Sorts.insertion(increasing);
+    System.out.println( "ArrayList increasing after sorting:\n" + increasing );
+    System.out.println("======================");
+    // == Worst Case Scenario == //
+    // 10 swaps neccessary
+
+    System.out.println( "ArrayList decreasing before sorting:\n" + decreasing );
+    Sorts.insertion(decreasing);
+    System.out.println( "ArrayList decreasing after sorting:\n" + decreasing );
+    System.out.println("======================");
+    // == Unordered Case Scenario == //
+    // 5 swaps
+
+    System.out.println( "ArrayList unordered before sorting:\n" + unordered );
+    Sorts.insertion(unordered);
+    System.out.println( "ArrayList unordered after sorting:\n" + unordered );
+    System.out.println("======================");
+
+
+    // selection sort =========================
+    // == Best Case == //
+    // 10 swaps neccessary
+    System.out.println("\n==SelectionSort==");
+
+    System.out.println( "ArrayList increasing before sorting:\n" + increasing );
+    Sorts.selection(increasing);
+    System.out.println( "ArrayList increasing after sorting:\n" + increasing );
+    System.out.println("======================");
+    // == Worst Case Scenario == //
+    // 10 swaps neccessary
+
+    System.out.println( "ArrayList decreasing before sorting:\n" + decreasing );
+    Sorts.selection(decreasing);
+    System.out.println( "ArrayList decreasing after sorting:\n" + decreasing );
+    System.out.println("======================");
+    // == Unordered Case Scenario == //
+    // 10 swaps
+
+    System.out.println( "ArrayList unordered before sorting:\n" + unordered );
+    Sorts.selection(unordered);
+    System.out.println( "ArrayList unordered after sorting:\n" + unordered );
+    System.out.println("======================");
+
+
+  } // main
+} // Driver class
--- Sorts.java
+++ Sorts.java
@@ -1,25 +1,46 @@
 import java.util.ArrayList;
 public class Sorts {
 
-  public static void bubble(ArrayList<Comparable> data) {
+  public static void bubbleSortV(ArrayList<Comparable> data) {
+    int counter = 0;
     int len = data.size();
     		int passNum = 0;
     		while (passNum < len) {
     			for (int i = len-1;i>0;i--) {
     				if (data.get(i).compareTo(data.get(i-1)) < 0){ //need to swap
     					data.set( i, data.set(i-1,data.get(i)) );
+
+              counter += 1;
+
     				}
     			}
     			passNum += 1;
     		}
+        System.out.println("Number of swaps: " + counter);
   } // bubble
 
-  public static void insertion(ArrayList<Comparable> data) {
+  public static ArrayList<Comparable> bubble( ArrayList<Comparable> input ) {
+    //declare and initialize empty ArrayList for copying
+    ArrayList<Comparable> data = new ArrayList<Comparable>();
+
+    //copy input ArrayList into working ArrayList
+    for( Comparable o : input )
+      data.add( o );
+
+    //sort working ArrayList
+    bubbleSortV( data );
+
+    //return working ArrayList
+    return data;
+  }//end bubbleSort
+
+  public static void insertionSortV(ArrayList<Comparable> data) {
+    int counter = 0;
     for(int partition = 1; partition < data.size(); partition++) {
       //partition marks first item in unsorted region
 
-      System.out.println( "\npartition: " + partition + "\ndataset: "); //diag
-      System.out.print( data +"\n");
+      // System.out.println( "\npartition: " + partition + "\ndataset: "); //diag
+      // System.out.print( data +"\n");
 
 
       //traverse sorted region from right to left
@@ -28,33 +49,67 @@ public class Sorts {
         // "walk" the current item to where it belongs
         // by swapping adjacent items
         if (data.get(i).compareTo(data.get(i-1)) < 0) {
-          System.out.println( "swap indices "+(i-1)+" & "+i+"..." ); //diag
+          // System.out.println( "swap indices "+(i-1)+" & "+i+"..." ); //diag
 	        data.set( i, data.set(i-1,data.get(i)) );
 
+          counter += 1;
         }
         else
           break;
       }
     }
+      System.out.println("Number of swaps: " + counter);
   } // insertion
 
-  public static void selection(ArrayList<Comparable> data) {
+  public static ArrayList<Comparable> insertion( ArrayList<Comparable> input ) {
+    //declare and initialize empty ArrayList for copying
+    ArrayList<Comparable> data = new ArrayList<Comparable>();
+
+    //copy input ArrayList into working ArrayList
+    for( Comparable o : input )
+      data.add( o );
+
+    //sort working ArrayList
+    insertionSortV( data );
+
+    //return working ArrayList
+    return data;
+  }//end insertionSort
+
+  public static void selectionSortV(ArrayList<Comparable> data) {
     int maxPos;
+    int counter = 0;
 
         for( int pass = data.size()-1; pass > 0; pass-- ) {
-          System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
+          // System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
           maxPos = 0;
           for( int i = 1; i <= pass; i++ ) {
-            System.out.println( "maxPos: " + maxPos );//diag
-            System.out.println( data );//diag
+            // System.out.println( "maxPos: " + maxPos );//diag
+            // System.out.println( data );//diag
             if ( data.get(i).compareTo( data.get(maxPos) ) > 0 )
               maxPos = i;
+              counter += 1;
           }
           data.set( maxPos, ( data.set( pass, data.get(maxPos) ) ) );
-          System.out.println( "after swap: " +  data );//diag
+          // System.out.println( "after swap: " +  data );//diag
         }
-
+        System.out.println("Number of swaps: " + counter);
   } // selection
+
+  public static ArrayList<Comparable> selection( ArrayList<Comparable> input ) {
+    //declare and initialize empty ArrayList for copying
+    ArrayList<Comparable> data = new ArrayList<Comparable>();
+
+    //copy input ArrayList into working ArrayList
+    for( Comparable o : input )
+      data.add( o );
+
+    //sort working ArrayList
+    selectionSortV( data );
+
+    //return working ArrayList
+    return data;
+  }//end insertionSort
 
   public static void main( String [] args ) {
       ArrayList glen = new ArrayList<Integer>();
